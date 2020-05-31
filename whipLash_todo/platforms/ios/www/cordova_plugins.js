81c96d29a2428432add455d0b3ebb89baf3dfc4b
KUNISHIMA ATSUSHI <kuniatsu@mbp.local>
2020-06-01 00:58:50 +0900

    iosとandroidのemulaterで動作確認

--- whipLash_todo/platforms/ios/www/cordova_plugins.js
+++ whipLash_todo/platforms/ios/www/cordova_plugins.js
@@ -1,6 +1,25 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [];
+  module.exports = [
+    {
+      "id": "cordova-plugin-insomnia.Insomnia",
+      "file": "plugins/cordova-plugin-insomnia/www/Insomnia.js",
+      "pluginId": "cordova-plugin-insomnia",
+      "clobbers": [
+        "window.plugins.insomnia"
+      ]
+    },
+    {
+      "id": "cordova-plugin-tts.tts",
+      "file": "plugins/cordova-plugin-tts/www/tts.js",
+      "pluginId": "cordova-plugin-tts",
+      "clobbers": [
+        "TTS"
+      ]
+    }
+  ];
   module.exports.metadata = {
+    "cordova-plugin-insomnia": "4.3.0",
+    "cordova-plugin-tts": "0.2.3",
     "cordova-plugin-whitelist": "1.3.4"
   };
 });
