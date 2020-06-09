67899257fb6109afee4fd967a53c3cf355ed640b
KUNISHIMA ATSUSHI <kuniatsu@mbp.local>
2020-06-09 14:07:16 +0900

    insomniaがうまくいかない問題発生中

--- whipLash_todo/build.sh
+++ whipLash_todo/build.sh
@@ -1,7 +1,9 @@
 rm -rf platforms/android
 rm -rf platforms/ios
-cordova platform add android
+# cordova platform add android
 cordova platform add ios
+cordova plugin add https://github.com/EddyVerbruggen/Insomnia-PhoneGap-Plugin.git
+cordova prepare
 cordova build 
 # cordova emulate ios
-echo emulater up -> $ cordovaemulate ios
+# echo emulater up -> $ cordovaemulate ios
